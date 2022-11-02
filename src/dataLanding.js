a8d708cb3e04b58645c5b30332f2046c701d01a3
Miguel González <miguel.gf.130@gmail.com>
2022-11-02 13:13:08 -0600

    Se crea archivo para almacenar data dinámica

---
+++ src/dataLanding.js
@@ -0,0 +1,30 @@
+const carouselItems = [
+  {
+    "image_name": "imagen_lorem.jpg",
+    "caption_title": "First item",
+    "caption_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elitd"
+  },
+  {
+    "image_name": "imagen_lorem.jpg",
+    "caption_title": "Ten item",
+    "caption_description": "Lorem hello world"
+  },
+]
+
+const experienceItems = [
+  {
+    "key": 0,
+    "title": "Actually - 2022",
+    "description": "First description of experience"
+  },
+  {
+    "key": 1,
+    "title": "Last one - 2021",
+    "description": "Second description of experience"
+  },
+]
+
+export {
+  carouselItems,
+  experienceItems
+}
